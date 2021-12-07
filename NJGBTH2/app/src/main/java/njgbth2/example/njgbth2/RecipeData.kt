165ca2ebf9dcdfd76e5d74a58d5c74dfcd4e66fe
ANIHANBIN <ehanbin98@gmail.com>
2021-12-07 16:53:41 +0900

    Revert "기초뼈대파일"
    This reverts commit 282833b699a1fe3bd8d2af9f5235ce1496827c37.

--- NJGBTH2/app/src/main/java/njgbth2/example/njgbth2/RecipeData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.njgbth2
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
-data class RecipeData(
-    val name : String,val numOFHeart : Int) {
-
-}
---
+++ app/src/main/java/com/example/njgbth/MainPage.kt
@@ -0,0 +1,45 @@
+package com.example.njgbth
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
+import android.widget.TextView
+import com.example.njgbth.databinding.ActivityMainPageBinding
+
+class MainPage : AppCompatActivity() {
+    private lateinit var binding: ActivityMainPageBinding           //viewbinding
+    var catename="전체"       //선택된 카테고리 이름
+    var cateid=0            //선택된 카테고리 id
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainPageBinding.inflate(layoutInflater)   //viewbinding
+        setContentView(binding.root)                                //화면출력
+        binding.T1.setBackgroundColor(Color.LTGRAY)                 //초기선택값 색설정
+        cateid=binding.T1.id                                        //초기 cateid 설정
+        binding.searchIngredient.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {  //검색 눌렀을 때
+                if(query!=null)                                 //검색내용이 있을 때
+                    println(query)
+                return true
+            }
+            override fun onQueryTextChange(newText: String?): Boolean { //검색창 수정될 때
+                return true
+            }
+        })
+    }
+    fun get_category(view: View) {              //textview onclick
+        //println(binding.T2.text)              T2의 text값 출력
+        if(cateid!=view.id){
+            findViewById<TextView>(cateid).setBackgroundColor(Color.WHITE)  //기존선택값 색변경
+            cateid=view.id                                                  //cateid 재설정
+            catename=findViewById<TextView>(cateid).text.toString()         //catename 재설정
+            findViewById<TextView>(cateid).setBackgroundColor(Color.LTGRAY)     //새로운 선택값 색 변경
+        }
+        //println(findViewById<TextView>(cateid).text)          //cateid의 text값 출력
+
+    }
+
+}

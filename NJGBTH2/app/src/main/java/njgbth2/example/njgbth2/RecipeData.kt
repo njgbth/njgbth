1f72568c785f8dc19504017ccd1ae42b987cc2a4
ANIHANBIN <ehanbin98@gmail.com>
2021-12-07 17:48:47 +0900

    기본

---
+++ NJGBTH2/app/src/main/java/njgbth2/example/njgbth2/RecipeData.kt
@@ -0,0 +1,9 @@
+package com.example.njgbth2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+data class RecipeData(
+    val name : String,val numOFHeart : Int) {
+
+}
